894b12ac09da53c7b4d7677c6f8db304379ca676
Dangtrinh28 <nguyencaodangtrinhtv3@gmail.com>
2022-11-25 13:37:20 +0700

    Thuy Moc food

---
+++ .history/assets/js/index_20221121233818.js
@@ -0,0 +1,113 @@
+(function ($) {
+  "use strict";
+
+  // wow js
+  new WOW().init();
+
+  //counter-up
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
+
+  $(document).ready(function () {
+    var $videoSrc;
+    $(".btn-play").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    // console.log($videoSrc);
+
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
+
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#video").attr("src", $videoSrc);
+    });
+  });
+
+  // testimonial-carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        item: 3,
+      },
+    },
+  });
+  //
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 100, "easeInOutExpo");
+    return false;
+  });
+})(jQuery);
+
+// function sendEmail() {
+  // var name = document.getElementById("name").value;
+  // var number = document.getElementById("number").value;
+  // var email = document.getElementById("email").value;
+  // var quality = document.getElementById("quality").value;
+  // var time = document.getElementById("time").value;
+  // var date = document.getElementById("date").value;
+  // var message = document.getElementById("message").value;
+
+//   Email.send({
+//     secureToken: "7b066fee-87fc-4581-a29b-e84ad506ffc2",
+    // pass xcliahnbanukicoz
+//     To: "nguyencaodangtrinhtv3@gmail.com",
+//     From: "dtrinhit04@gmail.com",
+//     Subject: "This is the subject",
+//     Body: `Tên khách hàng: ${name} <br> Số điện thoại: ${number} <br> Tên gmail của quý khách; ${Email} <br> Số lượng khách hàng: ${quality} <br> 
+//  Thời gian đặt bàn: ${time} <br> Ngày đặt bàn: ${date} <br> Yêu cầu của khách hàng: ${message}`,
+//   })
+//   .then((message) => alert(message));
+// }
+
+function sendEmail() {
+  console.log("say HI")
+
+
+  
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "nguyencaodangtrinhtv3@gmail.com",
+    Password : "B2145E9AB344FD4C1D449007775D22DCDDA7",
+    To : 'dtrinhit04@gmail.com',
+    From : "nguyencaodangtrinhtv3@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
---
+++ .history/assets/js/index_20221122075522.js
@@ -0,0 +1,123 @@
+(function ($) {
+  "use strict";
+
+  // wow js
+  new WOW().init();
+
+  //counter-up
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
+
+  $(document).ready(function () {
+    var $videoSrc;
+    $(".btn-play").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    // console.log($videoSrc);
+
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
+
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#video").attr("src", $videoSrc);
+    });
+  });
+
+  // testimonial-carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        item: 3,
+      },
+    },
+  });
+  //
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 100, "easeInOutExpo");
+    return false;
+  });
+})(jQuery);
+
+// function sendEmail() {
+  // var name = document.getElementById("name").value;
+  // var number = document.getElementById("number").value;
+  // var email = document.getElementById("email").value;
+  // var quality = document.getElementById("quality").value;
+  // var time = document.getElementById("time").value;
+  // var date = document.getElementById("date").value;
+  // var message = document.getElementById("message").value;
+
+//   Email.send({
+//     secureToken: "7b066fee-87fc-4581-a29b-e84ad506ffc2",
+    // pass xcliahnbanukicoz
+//     To: "nguyencaodangtrinhtv3@gmail.com",
+//     From: "dtrinhit04@gmail.com",
+//     Subject: "This is the subject",
+//     Body: `Tên khách hàng: ${name} <br> Số điện thoại: ${number} <br> Tên gmail của quý khách; ${Email} <br> Số lượng khách hàng: ${quality} <br> 
+//  Thời gian đặt bàn: ${time} <br> Ngày đặt bàn: ${date} <br> Yêu cầu của khách hàng: ${message}`,
+//   })
+//   .then((message) => alert(message));
+// }
+
+function sendEmail() {
+  let useNames = document.getElementById("names").value;
+  let numberPhone = document.getElementById("numberPhone").value;
+  let email = document.getElementById("email").value;
+  let qualitys = document.getElementById("qualitys").value;
+  let times = document.getElementById("times").value;
+  let date = document.getElementById("date").value;
+  let messages = document.getElementById("messages").value;
+  let body = useNames + numberPhone + email + qualitys + times + date + messages ;
+
+  let boody: `Tên khách hàng: ${name} <br> Số điện thoại: ${number} <br> Tên gmail của quý khách; ${Email} <br> Số lượng khách hàng: ${quality} <br> 
+//  Thời gian đặt bàn: ${time} <br> Ngày đặt bàn: ${date} <br> Yêu cầu của khách hàng: ${message}`,
+
+
+
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "nguyencaodangtrinhtv3@gmail.com",
+    Password : "B2145E9AB344FD4C1D449007775D22DCDDA7",
+    To : 'nguyencaodangtrinhtv3@gmail.com',
+    From : "nguyencaodangtrinhtv3@gmail.com",
+    Subject : "Đặt bàn online tại Thủy Mộc for web",
+    Body : body,
+}).then(
+  message => alert(message)
+);
+}
